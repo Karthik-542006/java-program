6a0788a06f2cb0b2cdbcc766501436ce2a0b62ab
Karthik-542006 <tskarthik542006@gmail.com>
2026-07-15 11:14:37 +0530

    Add files via upload

---
+++ karthik java1.java
@@ -0,0 +1,10 @@
+public class Main
+ {
+      publicSystem.out.printlnString[] args) {
+           int number = 29;
+     if (number % 2 == 0)
+              System.out.println(number + " is Even");
+     else
+              System.out.println(number + " is odd");
+      }
+ }
